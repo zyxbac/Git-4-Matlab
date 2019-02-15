7ccf0c06b99a0ae88d19cdb60b7a4b9d918e6e81
Michael Breuer <michael.breuer@ilsf.de>
2019-02-15 12:45:20 +0100

    changed metadata for upcoming release

--- tbx/Git 4 Matlab/Contents.m
+++ tbx/Git 4 Matlab/Contents.m
@@ -1,8 +1,9 @@
 % Git 4 Matlab
-% Version 1.0 (R2016b) 10-Oct-2018
+% Version 1.1 (R2018b) 15-Feb-2019
 %
 % Functions
 %   git                - executes git commands oon command line.
 %   gitFileIsUnchanged - check wether a file is in clean state.
+%   gitRevision        - returns revision id of HEAD
 %
 %  Copyright 2018 Michael Breuer
